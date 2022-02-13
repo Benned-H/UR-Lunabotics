99078f7f6aa1d92cfb946867e0ad31d91cc7ea60
redrn <58347046+redrn@users.noreply.github.com>
2022-02-13 19:45:03 +0000

    rewrite Costmap class

--- mapper/include/mapper/costmap.h
+++ mapper/include/mapper/costmap.h
@@ -1,72 +1,12 @@
-#pragma once
+#ifndef COSTMAP_H
+#define COSTMAP_H
 
-#include <string>
 #include <vector>
-#include <iostream>
+#include <string>
 
-class CostMap {
-// TODO - Create separate OccupancyGrid class to clean up which functions are where
-// TODO - Rename this class to something clearer
 
-public:
-  /*
-    OccMapper class constructor
-    TODO - Redo this documentation using Doxygen
-
-    resolutionArg - Resolution (m) of the cost map
-    mapWidthArg - Width (number of cells) of the cost map
-    mapHeightArg - Height (number of cells) of the cost map
-    origin - Origin of the cost map
-    obstacleWidthArg - Presumed width (m) of a detected obstacle
-    thresholdArg - Threshold of occupancy over which a cell is deemed occupied
-    p0, p_free, p_occ - Probability parameters of the occupancy grid mapping algorithm
-  */
-  CostMap(const double, const double, 
-				const double, const unsigned int, 
-				const unsigned int);
-		
-  virtual ~CostMap() = default; // Default deconstructor
-
-  /**
-   * Converts some x coordinate in the global frame to a column in the costmap.
-   *
-   * @brief Converts some x coordinate in the global frame to a column in the costmap.
-   * @param[in]		x	x value to convert
-   * @returns		corresponding column as an int
-   */
-  int x_to_col(const double);
-  
-  /**
-   * Converts some y coordinate in the global frame to a row in the costmap.
-   *
-   * @brief Converts some y coordinate in the global frame to a row in the costmap.
-   * @param[in]		y	y value to convert
-   * @returns		corresponding column as an int
-   */
-  int y_to_row(const double);
-
-  /**
-   * TODO
-   */ 
-  int point_to_index(const double, const double);
-
-  int length();
-
-  bool in_map(const double x, const double);
-
-  /**
-   * Set TODO
-   */
-  void set(const double, const double, const double);
-
-  /**
-   * Get TODO
-   */
-  double get(const double, const double);
-
-  std::string to_string() const;
-
-protected:
+class CostMap
+{
 private:
   /* The size (m) of the side of each cell in the costmap */
   double resolution;
@@ -78,22 +18,66 @@ private:
   double min_y;
 
   /* Number of rows in the costmap */
-  int rows;
+  unsigned int rows;
 
   /* Number of columns in the costmap */
-  int cols;
+  unsigned int cols;
 
   std::vector<double> cost_vector;
-};
-/* Ex:           col#
-        {     0  1  2  3
-          0  {1, 2, 3, 4},
-    row#  1  {5, 6, 7, 8}
-          2  {9, 10, 11, 12}
-        }
-        |
-        |
-        -> {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12}
-*/
 
-std::ostream &operator<<(std::ostream &os, const CostMap &map);
+public:
+  /**
+   * OccMapper class constructor
+   * TODO - Redo this documentation using Doxygen
+
+   * resolutionArg - Resolution (m) of the cost map
+   * mapWidthArg - Width (number of cells) of the cost map
+   * mapHeightArg - Height (number of cells) of the cost map
+   * origin - Origin of the cost map
+   * obstacleWidthArg - Presumed width (m) of a detected obstacle
+   * thresholdArg - Threshold of occupancy over which a cell is deemed occupied
+   * p0, p_free, p_occ - Probability parameters of the occupancy grid mapping algorithm
+   */
+  CostMap (const double resolutionArg, const double min_xArg,
+              const double min_yArg, const unsigned int rowsArg,
+              const unsigned int colsArg);
+
+  /**
+   * Converts some x coordinate in the global frame to a column in the costmap.
+   *
+   * @brief Converts some x coordinate in the global frame to a column in the costmap.
+   * @param in		x	x value to convert
+   * @returns		corresponding column as an int
+   */
+  int x_to_col(const double x) const;
+  
+  /**
+   * Converts some y coordinate in the global frame to a row in the costmap.
+   *
+   * @brief Converts some y coordinate in the global frame to a row in the costmap.
+   * @param in		y	y value to convert
+   * @returns		corresponding column as an int
+   */
+  int y_to_row(const double y) const;
+
+  /**
+   * TODO
+   */ 
+  int point_to_index(const double x, const double y) const;
+
+  int length() const;
+
+  bool in_map(const double x, const double y) const;
+
+  void set(const double x, const double y, const double value);
+
+  double get(const double x, const double y) const;
+
+  std::string to_string () const;
+
+};
+
+std::ostream &operator<< (std::ostream &os, const CostMap &map);
+
+
+#endif //COSTMAP_H
--- mapper/src/mapper/costmap.cpp
+++ mapper/src/mapper/costmap.cpp
@@ -1,60 +1,33 @@
-// Author: Benned Hedegaard
-
-#include <cmath>
-#include <iostream>
-#include <vector>
-
 #include "mapper/costmap.h"
+#include <cmath>
 
-/*
-	OccMapper class constructor
-
-	resolutionArg - Resolution (m) of the cost map
-	mapWidthArg - Width (number of cells) of the cost map
-	mapHeightArg - Height (number of cells) of the cost map
-	origin - Origin of the cost map
-	obstacleWidthArg - Presumed width (m) of a detected obstacle
-	thresholdArg - Threshold of occupancy over which a cell is deemed occupied
-	p0, p_free, p_occ - Prior, free, and occupied probabilities of cells given laser hit
-*/
-CostMap::CostMap(const double resolutionArg, const double min_xArg, 
-				const double min_yArg, const unsigned int rowsArg, 
-				const unsigned int colsArg) {
-  // TODO - Correctly initialize all class member variables
-	
-	resolution = resolutionArg;
-
-	min_x = min_xArg;
-	min_y = min_yArg;
-
-	rows = rowsArg;
-	cols = colsArg;
-
-	cost_vector = std::vector<double>(rows * cols);
-	//can remodify if you want to pass in arguments for these values in advanced
-}
+CostMap::CostMap (const double resolutionArg, const double min_xArg,
+                  const double min_yArg, const unsigned int rowsArg,
+                  const unsigned int colsArg) 
+                  : resolution{resolutionArg}, min_x{min_xArg}, min_y{min_yArg}, rows{rowsArg},cols{colsArg}, cost_vector{std::vector<double>(cols * rows, 0)}
+{}
 
 // Returns the map's column index for the given x coordinate.
-int CostMap::x_to_col(const double x) {
-	return floor((x - min_x) / resolution);
+int CostMap::x_to_col(const double x) const {
+  return floor((x - min_x) / resolution);
 }
 
 // Returns the map's row index for the given y coordinate.
-int CostMap::y_to_row(const double y) {
-		return floor((y - min_y) / resolution);
+int CostMap::y_to_row(const double y) const {
+	return floor((y - min_y) / resolution);
 }
 
 // Returns the index in the grid of the given (x,y) point.
-int CostMap::point_to_index(const double x, const double y) {
+int CostMap::point_to_index(const double x, const double y) const {
 	return cols * y_to_row(y) + x_to_col(x);
 }
 
-int CostMap::length() {
+int CostMap::length() const {
 	return cost_vector.size();
 }
 
 // Returns if the given (x,y) point is in the map's range.
-bool CostMap::in_map(const double x, const double y) {
+bool CostMap::in_map(const double x, const double y) const {
 	return min_x <= x && x <= min_x + cols * resolution 
 		&& min_y <= y && y <= min_y + rows * resolution;
 }
@@ -63,7 +36,7 @@ void CostMap::set(const double x, const double y, const double value) {
 	cost_vector[y_to_row(y) * cols + x_to_col(x)] = value;
 }
 
-double CostMap::get(const double x, const double y) {
+double CostMap::get(const double x, const double y) const {
 	return cost_vector[y_to_row(y) * cols + x_to_col(x)];
 }
 
