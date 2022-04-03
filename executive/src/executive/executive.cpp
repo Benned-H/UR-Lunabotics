46ee31927d4d8641a6d06269f44c60699de7b448
Francesca Daszak <fdaszak@u.rochester.edu>
2022-04-03 15:10:54 -0400

    Draft executive state machine.

--- executive/src/executive/executive.cpp
+++ executive/src/executive/executive.cpp
@@ -11,10 +11,70 @@
 Executive::Executive( const double& reachedArg, const double& replanArg ) :
     reached_distance( reachedArg ), replan_distance( replanArg ), hasOdom( false ){}
 
+//Create an enum to track states.
+enum State{Initialize, ManualControl, Localize, GoToMine, SetupMine, Mine, SetupDeposit, Deposit, Terminate};
+
 // TODO - Move to a common package instead
 double euclidean( double x1, double y1, double x2, double y2 ){
 	return std::sqrt( (x1-x2)*(x1-x2) + (y1-y2)*(y1-y2) );
 }
+
+void Executive::stateMachine(){
+	State curr = Initialize;
+	bool run = true;
+	while(run){
+		switch(curr){
+			case Initialize:
+				//returns Localize, ManualControl, or Terminate
+				curr = Initialize();	
+				break;
+			case ManualControl:
+				//code here
+				break;
+			case Localize:
+				//code here
+				break;
+			case GoToMine:
+				//code here
+				break;
+			case SetupMine:
+				//code here
+				break;
+			case Mine:
+				//code here
+				break;
+			case SetupMine:
+				//code here
+                	        break;
+			case Mine:
+				//code here
+                	        break;
+			case SetupDeposit:
+				//code here
+                	        break;
+			case Deposit:
+				//code here
+                	        break;
+			case Terminate:
+				//code here
+				run = false;
+                	        break;
+		}
+	}
+
+}
+
+State Executive::Initialize(){
+	//bool are motors connected?
+	//bool is camera connected?
+	//bool is telemetry signal working?
+	// if motors connected, camera connected
+		//if telemetry works return Localize
+		//else try signal again, if fails return Terminate
+	//else if motors connected
+		//check telemetry - if works then return ManualControl
+}
+
 
 void Executive::handleOdom( const nav_msgs::Odometry::ConstPtr& msg ){
 	state = *msg;
