adce33f88b09ac2afb7c4c1c63ea52c9122423b5
redrn <58347046+redrn@users.noreply.github.com>
2022-04-03 13:46:03 -0400

    adapts planner to new msg and Node class

--- planner/src/planner/grid-planner.cpp
+++ planner/src/planner/grid-planner.cpp
@@ -41,14 +41,14 @@ bool compNodes( std::shared_ptr<Node>& n1, std::shared_ptr<Node>& n2 ){
 std::vector< std::shared_ptr<Node> > getNeighbors( std::shared_ptr<Node>& curr ){
   std::vector< std::shared_ptr<Node> > neighbors;
 
-  neighbors.push_back( std::make_shared<Node>( curr->x+1, curr->y ) );
-  neighbors.push_back( std::make_shared<Node>( curr->x+1, curr->y+1 ) );
-  neighbors.push_back( std::make_shared<Node>( curr->x, curr->y+1 ) );
-  neighbors.push_back( std::make_shared<Node>( curr->x-1, curr->y+1 ) );
-  neighbors.push_back( std::make_shared<Node>( curr->x-1, curr->y ) );
-  neighbors.push_back( std::make_shared<Node>( curr->x-1, curr->y-1 ) );
-  neighbors.push_back( std::make_shared<Node>( curr->x, curr->y-1 ) );
-  neighbors.push_back( std::make_shared<Node>( curr->x+1, curr->y-1 ) );
+  neighbors.push_back( std::make_shared<Node>( curr->i_x+1, curr->i_y ) );
+  neighbors.push_back( std::make_shared<Node>( curr->i_x+1, curr->i_y+1 ) );
+  neighbors.push_back( std::make_shared<Node>( curr->i_x, curr->i_y+1 ) );
+  neighbors.push_back( std::make_shared<Node>( curr->i_x-1, curr->i_y+1 ) );
+  neighbors.push_back( std::make_shared<Node>( curr->i_x-1, curr->i_y ) );
+  neighbors.push_back( std::make_shared<Node>( curr->i_x-1, curr->i_y-1 ) );
+  neighbors.push_back( std::make_shared<Node>( curr->i_x, curr->i_y-1 ) );
+  neighbors.push_back( std::make_shared<Node>( curr->i_x+1, curr->i_y-1 ) );
 
   return neighbors;
 }
@@ -62,7 +62,7 @@ std::vector<geometry_msgs::Point> GridPlanner::aStar( const geometry_msgs::Point
 	// Start from the closest on-grid point.
 	std::shared_ptr<Node> s0 = std::make_shared<Node>( std::round(start.x / DISCRETIZATION), std::round(start.y / DISCRETIZATION) );
 	s0->g = 0.0;
-	s0->f = euclidean( s0->x, s0->y, goal.x, goal.y );
+	s0->f = euclidean( s0->i_x, s0->i_y, goal.x, goal.y );
 	open_list.push( s0 );
 	
 	std_msgs::UInt32 open_list_size; // For debugging purposes.
@@ -83,21 +83,21 @@ std::vector<geometry_msgs::Point> GridPlanner::aStar( const geometry_msgs::Point
 		closed_list.push_back( curr );
 
 		// If we've almost reached the goal, extract and return the path.
-		if( euclidean( (curr->x)*DISCRETIZATION, (curr->y)*DISCRETIZATION, goal.x, goal.y ) <= DISCRETIZATION ){
+		if( euclidean( (curr->i_x)*DISCRETIZATION, (curr->i_y)*DISCRETIZATION, goal.x, goal.y ) <= DISCRETIZATION ){
 			std::vector<geometry_msgs::Point> path;
-			if( ( DISCRETIZATION*curr->x != goal.x ) || ( DISCRETIZATION*curr->y != goal.y ) ){
+			if( ( DISCRETIZATION*curr->i_x != goal.x ) || ( DISCRETIZATION*curr->i_y != goal.y ) ){
 				path.push_back( goal ); // Include goal as final path point if Node didn't precisely reach it.
 			}
 			geometry_msgs::Point p;
-			p.x = DISCRETIZATION*curr->x;
-			p.y = DISCRETIZATION*curr->y;
+			p.x = DISCRETIZATION*curr->i_x;
+			p.y = DISCRETIZATION*curr->i_y;
 			path.insert( path.begin(), p );
 
 			while( curr->prev != nullptr ){
         curr = curr->prev;
         geometry_msgs::Point p;
-        p.x = DISCRETIZATION*curr->x;
-        p.y = DISCRETIZATION*curr->y;
+        p.x = DISCRETIZATION*curr->i_x;
+        p.y = DISCRETIZATION*curr->i_y;
         path.insert( path.begin(), p );
       }
 
@@ -110,8 +110,8 @@ std::vector<geometry_msgs::Point> GridPlanner::aStar( const geometry_msgs::Point
     for( int i = 0; i < neighbors.size(); i++ ){
       auto new_node = neighbors[i];
 			
-      if( cost_map.occupied( DISCRETIZATION*new_node->x, DISCRETIZATION*new_node->y ) ){
-        std::cout << "Node was occupied: (" << (DISCRETIZATION*new_node->x) << "," << (DISCRETIZATION*new_node->y) << ")" << std::endl;
+      if( cost_map.occupied( DISCRETIZATION*new_node->i_x, DISCRETIZATION*new_node->i_y ) ){
+        std::cout << "Node was occupied: (" << (DISCRETIZATION*new_node->i_x) << "," << (DISCRETIZATION*new_node->i_y) << ")" << std::endl;
         continue;
       }
 			
@@ -119,7 +119,7 @@ std::vector<geometry_msgs::Point> GridPlanner::aStar( const geometry_msgs::Point
 			bool closed = false;
 			for( int c = 0; c < closed_list.size(); c++ ){
         // TODO - Node equality function on discrete indices
-				if( closed_list[c]->x == new_node->x && closed_list[c]->y == new_node->y ){
+				if( closed_list[c]->i_x == new_node->i_x && closed_list[c]->i_y == new_node->i_y ){
 				  closed = true;
 				  break;
 				}
@@ -130,8 +130,8 @@ std::vector<geometry_msgs::Point> GridPlanner::aStar( const geometry_msgs::Point
 			}
 
 			// Set new node's g and f values and its previous node.
-			new_node->g = curr->g + discrete_euclidean( new_node->x, new_node->y, curr->x, curr->y, DISCRETIZATION );
-			new_node->f = new_node->g + euclidean( DISCRETIZATION*new_node->x, DISCRETIZATION*new_node->y, goal.x, goal.y );
+			new_node->g = curr->g + discrete_euclidean( new_node->i_x, new_node->i_y, curr->i_x, curr->i_y, DISCRETIZATION );
+			new_node->f = new_node->g + euclidean( DISCRETIZATION*new_node->i_x, DISCRETIZATION*new_node->i_y, goal.x, goal.y );
 			new_node->prev = curr;
 
 			// Push to the open list.
